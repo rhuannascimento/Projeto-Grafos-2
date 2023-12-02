9a0dfd81762c3f5eef444174707924fb2e91ece3
Rhuan Nascimento Ferreria <rhuan2414@gmail.com>
2023-12-02 18:41:23 -0300

    executar todas instancias de uma vez

--- main.cpp
+++ main.cpp
@@ -1,37 +1,51 @@
 #include <iostream>
 #include "./include/Problema.h"
 #include "./include/Solucao.h"
+#include <string>
 
 using namespace std;
 
+int main()
+{
 
-int main(){
+    for (int x = 1; x <= 10; x++)
+    {
+        cout<<"==========================================================Instancia " << x <<"==============================================================================================="<<endl;
+        Problema *p = new Problema("instancias/"+to_string(x)+".txt");
+        Solucao *s = new Solucao(p);
 
-    Problema *p = new Problema("instancias/10.txt");
-    Solucao *s = new Solucao(p);
+        vector<vector<int>> rotas = s->guloso();
 
-    vector<vector<int>> rotas = s->guloso();
-
-    for (int i = 0; i < rotas.size(); ++i) {
-        if(i == rotas.size()-1){
-            cout << "Custo: ";
-        }else{
-            cout << "Rota " << i + 1 << ": ";
-        }
-        for (int j = 0; j < rotas[i].size(); ++j) {
-            if(j == rotas[i].size() - 2 && !(i == rotas.size()-1)){
-                cout << "Ditancia: " << rotas[i][j] << " ";
-            }else if(j == rotas[i].size() - 1 && !(i == rotas.size()-1)){
-                cout << "Carga total da rota: " << rotas[i][j] << " ";
-            }else{
-                cout << rotas[i][j] << " ";
+        for (int i = 0; i < rotas.size(); ++i)
+        {
+            if (i == rotas.size() - 1)
+            {
+                cout << "Custo: ";
             }
+            else
+            {
+                cout << "Rota " << i + 1 << ": ";
+            }
+            for (int j = 0; j < rotas[i].size(); ++j)
+            {
+                if (j == rotas[i].size() - 2 && !(i == rotas.size() - 1))
+                {
+                    cout << "Ditancia: " << rotas[i][j] << " ";
+                }
+                else if (j == rotas[i].size() - 1 && !(i == rotas.size() - 1))
+                {
+                    cout << "Carga total da rota: " << rotas[i][j] << " ";
+                }
+                else
+                {
+                    cout << rotas[i][j] << " ";
+                }
+            }
+            cout << endl;
         }
-        cout << endl;
+
+        cout << "O resultado otimo é " << p->getSolucaoOtima() << endl;
+        cout << "O resultado atual é " << s->getComparacao() << " vezes pior que a solução ótima!" << endl << endl << endl;
     }
-
-    cout<< "O resultado otimo é " << p->getSolucaoOtima() << endl;
-    cout<< "O resultado atual é " << s->getComparacao() << " vezes pior que a solução ótima!" << endl;
-
     return 0;
 }
