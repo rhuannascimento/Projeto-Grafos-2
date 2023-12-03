256700397bfa6c4380ba6f71e921c6f75fc1a24d
Rhuan Nascimento Ferreria <rhuan2414@gmail.com>
2023-12-03 12:27:03 -0300

    commit de segurnaca

--- main.cpp
+++ main.cpp
@@ -19,60 +19,63 @@ int main()
         cout << "|3| Algoritmo Guloso randomizado adaptativo reativo" << endl;
         cout << "|9| Sair" << endl;
         cin >> menu;
-
-        for (int x = 1; x <= 10; x++)
+        if (menu!=9)
         {
-            cout << "==========================================================Instancia " << x << "===============================================================================================" << endl;
-            Problema *p = new Problema("instancias/" + to_string(x) + ".txt");
-            Solucao *s = new Solucao(p);
-            vector<vector<int>> rotas;
-            switch (menu)
-            {
-            case 1:
-                rotas = s->guloso();
-                break;
-            case 2:
-                rotas = s->guloso();
-                break;
-            case 3:
-                rotas = s->guloso();
-                break;
-            }
-            Valida *v = new Valida(s, p);
 
-            for (size_t i = 0; i < rotas.size(); ++i)
+            for (int x = 1; x <= 10; x++)
             {
-                cout << "Rota " << i + 1 << ": ";
-                for (size_t j = 0; j < rotas[i].size(); ++j)
+                cout << "==========================================================Instancia " << x << "===============================================================================================" << endl;
+                Problema *p = new Problema("instancias/" + to_string(x) + ".txt");
+                Solucao *s = new Solucao(p);
+                vector<vector<int>> rotas;
+                switch (menu)
                 {
-                    if (j == rotas[i].size() - 2)
-                    {
-                        cout << "Capacidade sobrando: ";
-                    }
-                    else if (j == rotas[i].size() - 1)
-                    {
-                        cout << "Custo da rota: ";
-                    }
+                case 1:
+                    rotas = s->guloso();
+                    break;
+                case 2:
+                    rotas = s->guloso();
+                    break;
+                case 3:
+                    rotas = s->gulosoReativo();
+                    break;
+                }
+                Valida *v = new Valida(s, p);
 
+                for (size_t i = 0; i < rotas.size(); ++i)
+                {
+                    cout << "Rota " << i + 1 << ": ";
+                    for (size_t j = 0; j < rotas[i].size(); ++j)
+                    {
+                        if (j == rotas[i].size() - 2)
+                        {
+                            cout << "Capacidade sobrando: ";
+                        }
+                        else if (j == rotas[i].size() - 1)
+                        {
+                            cout << "Custo da rota: ";
+                        }
+
+                        cout
+                            << rotas[i][j] << " ";
+                    }
                     cout
-                        << rotas[i][j] << " ";
+                        << endl;
                 }
                 cout
-                    << endl;
-            }
-            cout
-                << "Custo Total: " << s->getCustoTotal() << endl;
-            cout << "Solucao Otimo: " << p->getSolucaoOtima() << endl;
+                    << "Custo Total: " << s->getCustoTotal() << endl;
+                cout << "Solucao Otimo: " << p->getSolucaoOtima() << endl;
 
-            if (!v->validar())
-            {
-                if (!v->getAtendeuTodos())
+                if (!v->validar())
                 {
-                    cout << "A solução não atendeu todos os clientes!" << endl;
-                }
-                if (!v->getUnicoEmRota())
-                {
-                    cout << "Os nós não são únicos nas rotas!" << endl;
+                    if (!v->getAtendeuTodos())
+                    {
+                        cout << "A solução não atendeu todos os clientes!" << endl;
+                    }
+                    if (!v->getUnicoEmRota())
+                    {
+                        cout << "Os nós não são únicos nas rotas!" << endl;
+                    }
                 }
             }
         }
