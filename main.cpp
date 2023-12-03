b395abd52d68506c159c16776f2f28154852e677
igor <guedes.igor018@gmail.com>
2023-12-03 12:04:32 -0300

    menu interacao

--- main.cpp
+++ main.cpp
@@ -8,43 +8,76 @@ using namespace std;
 
 int main()
 {
+    int menu = 0;
 
-    for (int x = 1; x <= 10; x++)
+    cout << "escolha uma opcao de algoritmo:" << endl;
+    while (menu != 9)
     {
-        cout << "==========================================================Instancia " << x << "===============================================================================================" << endl;
-        Problema *p = new Problema("instancias/" + to_string(x) + ".txt");
-        Solucao *s = new Solucao(p);
-        vector<vector<int>> rotas = s->guloso();
-        Valida *v = new Valida(s, p);
+        cout << "====================================================================================================================================================" << endl;
+        cout << "escolha uma opcao de algoritmo:" << endl;
+        cout << "|1| Algoritmo guloso" << endl;
+        cout << "|2| Algoritmo Guloso randomizado adaptativo" << endl;
+        cout << "|3| Algoritmo Guloso randomizado adaptativo reativo" << endl;
+        cout << "|9| Sair" << endl;
+        cin >> menu;
 
-        for (size_t i = 0; i < rotas.size(); ++i)
+        switch (menu)
         {
-            cout << "Rota " << i + 1 << ": ";
-            for (size_t j = 0; j < rotas[i].size(); ++j)
+        case 1:
+            for (int x = 1; x <= 10; x++)
             {
-                if(j ==  rotas[i].size() - 2){
-                    cout<<"Capacidade sobrando: ";
-                }else if(j ==  rotas[i].size() - 1){
-                    cout<<"Custo da rota: ";
-                }
-                
-                cout << rotas[i][j] << " ";
-                
-            }
-            cout << endl;
-        }
-        cout<<"Custo Total: "<<s->getCustoTotal()<<endl;
-        cout<<"Solucao Otimo: "<<p->getSolucaoOtima()<<endl;
+                cout << "==========================================================Instancia " << x << "===============================================================================================" << endl;
+                Problema *p = new Problema("instancias/" + to_string(x) + ".txt");
+                Solucao *s = new Solucao(p);
+                vector<vector<int>> rotas = s->guloso();
+                Valida *v = new Valida(s, p);
 
-        if(!v->validar()){
-            if(!v->getAtendeuTodos()){
-                cout<<"A solução não atendeu todos os clientes!"<<endl;
+                for (size_t i = 0; i < rotas.size(); ++i)
+                {
+                    cout << "Rota " << i + 1 << ": ";
+                    for (size_t j = 0; j < rotas[i].size(); ++j)
+                    {
+                        if (j == rotas[i].size() - 2)
+                        {
+                            cout << "Capacidade sobrando: ";
+                        }
+                        else if (j == rotas[i].size() - 1)
+                        {
+                            cout << "Custo da rota: ";
+                        }
+
+                        cout
+                            << rotas[i][j] << " ";
+                    }
+                    cout
+                        << endl;
+                }
+                cout
+                    << "Custo Total: " << s->getCustoTotal() << endl;
+                cout << "Solucao Otimo: " << p->getSolucaoOtima() << endl;
+
+                if (!v->validar())
+                {
+                    if (!v->getAtendeuTodos())
+                    {
+                        cout << "A solução não atendeu todos os clientes!" << endl;
+                    }
+                    if (!v->getUnicoEmRota())
+                    {
+                        cout << "Os nós não são únicos nas rotas!" << endl;
+                    }
+                }
             }
-            if(!v->getUnicoEmRota()){
-                cout<<"Os nós não são únicos nas rotas!"<<endl;
-            }
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
         }
     }
+
     return 0;
-    
 }
